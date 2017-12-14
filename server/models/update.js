074533d02668c32380b836787984bce3a7184f23
Dustin Huang <mail@dustinhuang.com>
2017-12-13 16:49:42 -0800

    Change input key names

--- server/models/update.js
+++ server/models/update.js
@@ -1,11 +1,11 @@
 const db = require('../database/index.js');
 
 const views = function updateViewsRoute(req, res) {
-  req.body.views.forEach(({ id, viewCount }) => {
-    db.Statistics.findOne({ _id: { $eq: id } })
+  req.body.views.forEach(({ videoID, additionalViews }) => {
+    db.Statistics.findOne({ _id: { $eq: videoID } })
       .then((data) => {
         const doc = data;
-        doc.viewCount = (parseInt(data.viewCount, 10) + parseInt(viewCount, 10)).toString();
+        doc.viewCount = (parseInt(data.viewCount, 10) + parseInt(additionalViews, 10)).toString();
         doc.save();
       })
       .then(() => res.end());
