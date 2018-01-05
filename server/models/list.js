e3d19045a6a1e54f2e34913b2a2853ed2babee15
Dustin Huang <mail@dustinhuang.com>
2018-01-05 14:30:59 -0800

    Add changes for scaling on AWS EC2

--- server/models/list.js
+++ server/models/list.js
@@ -1,4 +1,11 @@
+const Promise = require('bluebird');
+const { createClient } = require('redis');
+const StatsD = require('node-statsd');
 const db = require('../database/index.js');
+
+const redis = createClient(6379, process.env.REDIS_URL);
+
+const statsd = new StatsD({ host: process.env.STATSD_URL, port: 8125 });
 
 const error = {
   error: {
@@ -16,91 +23,107 @@ const error = {
   },
 };
 
-const list = function getVideosById({ id, part }) {
-  return new Promise((resolve, reject) => {
-    if (id === undefined) {
+const list = function getVideosById({ id, part }, callback) {
+  if (id === undefined) {
+    const err = error;
+    err.error.errors[0].domain = 'youtube.parameter';
+    err.error.errors[0].reason = 'missingRequiredParameter';
+    err.error.errors[0].message = 'No filter selected. Expected: id';
+    err.error.errors[0].locationType = 'parameter';
+    err.error.errors[0].location = '';
+    err.error.message = 'No filter selected. Expected: id';
+    callback(err);
+    return;
+  }
+  if (part === undefined) {
+    const err = error;
+    err.error.errors[0].domain = 'global';
+    err.error.errors[0].reason = 'required';
+    err.error.errors[0].message = 'Required parameter: part';
+    err.error.errors[0].locationType = 'parameter';
+    err.error.errors[0].location = 'part';
+    err.error.message = 'Required parameter: part';
+    callback(err);
+    return;
+  }
+
+  const parts = part.split(/\s*,\s*/);
+
+  for (let i = 0; i < parts.length; i += 1) {
+    if (!['snippet', 'contentDetails', 'statistics', 'topicDetails'].some(e => e === parts[i])) {
       const err = error;
-      err.error.errors[0].domain = 'youtube.parameter';
-      err.error.errors[0].reason = 'missingRequiredParameter';
-      err.error.errors[0].message = 'No filter selected. Expected: id';
-      err.error.errors[0].locationType = 'parameter';
-      err.error.errors[0].location = '';
-      err.error.message = 'No filter selected. Expected: id';
-      reject(err);
-      return;
-    }
-    if (part === undefined) {
-      const err = error;
-      err.error.errors[0].domain = 'global';
-      err.error.errors[0].reason = 'required';
-      err.error.errors[0].message = 'Required parameter: part';
+      err.error.errors[0].domain = 'youtube.part';
+      err.error.errors[0].reason = 'unknownPart';
+      err.error.errors[0].message = parts[i] || '{0}';
       err.error.errors[0].locationType = 'parameter';
       err.error.errors[0].location = 'part';
-      err.error.message = 'Required parameter: part';
-      reject(err);
+      err.error.message = parts[i] || '{0}';
+      callback(err);
       return;
     }
+  }
 
-    const parts = part.split(/\s*,\s*/);
+  const ids = id.split(/\s*,\s*/);
 
-    for (let i = 0; i < parts.length; i += 1) {
-      if (!['snippet', 'contentDetails', 'statistics', 'topicDetails'].some(e => e === parts[i])) {
-        const err = error;
-        err.error.errors[0].domain = 'youtube.part';
-        err.error.errors[0].reason = 'unknownPart';
-        err.error.errors[0].message = parts[i] || '{0}';
-        err.error.errors[0].locationType = 'parameter';
-        err.error.errors[0].location = 'part';
-        err.error.message = parts[i] || '{0}';
-        reject(err);
-        return;
-      }
-    }
+  const responses = [];
 
-    const ids = id.split(/\s*,\s*/);
+  if (ids.length > 1) {
+    const match = { $match: { _id: { $in: ids } } };
+    const addFields = { $addFields: { __order: { $indexOfArray: [ids, '$_id'] } } };
+    const sort = { $sort: { __order: 1 } };
+    parts.forEach((prt) => {
+      responses.push(db.mongoose.model(prt).aggregate([match, addFields, sort]));
+    });
+  } else {
+    parts.forEach((prt) => {
+      responses.push(new Promise((res, rej) => {
+        const redisStart = new Date();
+        redis.get(`${id}.${prt}`, (err, reply) => {
+          if (err) {
+            rej(err);
+          } else if (reply) {
+            statsd.timing('redis_response_time', new Date() - redisStart, 0.1);
+            res(JSON.parse(reply));
+          } else {
+            const dbStart = new Date();
+            db.mongoose.model(prt).find({ _id: { $eq: ids[0] } }).limit(1)
+              .then((data) => {
+                redis.set(`${id}.${prt}`, JSON.stringify(data));
+                statsd.timing('mongo_response_time', new Date() - dbStart, 0.1);
+                res(data);
+              });
+          }
+        });
+      }));
+    });
+  }
 
-    const responses = [];
+  Promise.all(responses)
+    .then((data) => {
+      const response = {
+        kind: 'youtube#videoListResponse',
+        pageInfo: {
+          totalResults: data[0].length,
+          resultsPerPage: data[0].length,
+        },
+        Items: [],
+      };
 
-    if (ids.length > 1) {
-      const match = { $match: { _id: { $in: ids } } };
-      const addFields = { $addFields: { __order: { $indexOfArray: [ids, '$_id'] } } };
-      const sort = { $sort: { __order: 1 } };
-      parts.forEach((prt) => {
-        responses.push(db.mongoose.model(prt).aggregate([match, addFields, sort]));
-      });
-    } else {
-      parts.forEach((prt) => {
-        responses.push(db.mongoose.model(prt).find({ _id: { $eq: ids[0] } }).limit(1));
-      });
-    }
-
-    Promise.all(responses)
-      .then((data) => {
-        const response = {
-          kind: 'youtube#videoListResponse',
-          pageInfo: {
-            totalResults: data[0].length,
-            resultsPerPage: data[0].length,
-          },
-          Items: [],
-        };
-
-        if (data[0].length > 0) {
-          ids.forEach((vid, j) => {
-            const item = { id: vid };
-            parts.forEach((prt, i) => {
-              const pt = data[i][j];
-              delete pt.__order;
-              delete pt._id;
-              delete pt.__v;
-              item[prt] = data[i][j];
-            });
-            response.Items.push(item);
+      if (data[0].length > 0) {
+        ids.forEach((vid, j) => {
+          const item = { id: vid };
+          parts.forEach((prt, i) => {
+            const pt = data[i][j];
+            delete pt.__order;
+            delete pt._id;
+            delete pt.__v;
+            item[prt] = data[i][j];
           });
-        }
-        resolve(response);
-      });
-  });
+          response.Items.push(item);
+        });
+      }
+      callback(response);
+    });
 };
 
 module.exports = list;
--- server/models/update.js
+++ server/models/update.js
@@ -1,16 +1,14 @@
 const db = require('../database/index.js');
 
-const views = function updateViewsById(viewsData) {
-  return new Promise((resolve) => {
-    viewsData.forEach(({ videoID, additionalViews }) => {
-      db.Statistics.findOne({ _id: { $eq: videoID } })
-        .then((data) => {
-          const doc = data;
-          doc.viewCount = (parseInt(data.viewCount, 10) + parseInt(additionalViews, 10)).toString();
-          doc.save();
-        })
-        .then(() => resolve());
-    });
+const views = function updateViewsById(viewsData, callback) {
+  viewsData.forEach(({ videoID, additionalViews }) => {
+    db.Statistics.findOne({ _id: { $eq: videoID } })
+      .then((data) => {
+        const doc = data;
+        doc.viewCount = (parseInt(data.viewCount, 10) + parseInt(additionalViews, 10)).toString();
+        doc.save();
+      })
+      .then(() => callback());
   });
 };
 
